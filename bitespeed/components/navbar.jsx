6a35b8954a9767157e4b8eb2265cade4f713dee0
Govind P Lal <govindplal@gmail.com>
2024-05-18 23:36:32 +0530

    refactor: button

--- bitespeed/components/navbar.jsx
+++ bitespeed/components/navbar.jsx
@@ -3,12 +3,12 @@ import { Button } from './ui/button'
 const Navbar = ({saveFlow}) => {
   return (
     <div className='flex w-full py-2 px-[3vw] bg-slate-200 justify-end'>
-        <button
+        <Button
         variant = "outline" 
         onClick ={saveFlow}
         >
             Save changes
-        </button>
+        </Button>
     </div>
   )
 }
